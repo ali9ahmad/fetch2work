d16059f125135af64d692cd013a9ee697c014de3
ali9ahmad <temur.khan.dispatch@gmail.com>
2023-05-27 02:43:16 +0500

    good result

--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
-const elPostList = document.querySelector(".js-post-list");
-const API_PATH = "https://jsonplaceholder.typicode.com/photos#?_limit=100";
+const elItemResult = document.querySelector(".js-item-result");
+const elItemTemp = document.querySelector(".js-card-temp").content;
+const API_PATH = "https://jsonplaceholder.typicode.com/photos#";
 loading = true;
+newPostFragment = document.createDocumentFragment();
 
 function renderPost (data) {
-    data.forEach((item) => {
-        const newLi = document.createElement("li");
-        const newTitle = document.createElement("h3");
-        const newText = document.createElement("p")
-    }
+    data.slice(0, 10).forEach((item) => {
+        const cloneItem = elItemTemp.cloneNode(true);
+        
+        cloneItem.querySelector(".js-item-title").textContent = item.title;
+        cloneItem.querySelector(".js-item-photo").textContent = item.thumbnailUrl;
+        cloneItem.querySelector(".js-item-url").textContent = item.url;
+        
+        newPostFragment.appendChild(cloneItem);
+
+    });
+
+    elItemResult.appendChild(newPostFragment);
 }
 
 fetch (API_PATH)
 .then ((resorce) => resorce.json())
 .then ((data) => {
     console.log(data)
+    renderPost(data);
 })
 .catch ((error) => console.log(error))
 .finally (() => {
     console.log("fatch is done");
     loading = false;
-
+    
 });
 
